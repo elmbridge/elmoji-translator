a1dcfd8e56684cfa2c514991009ece03d8a4738f
Srinivas Rao <savinirs@gmail.com>
2016-10-01 12:33:27 -0700

    style with materialize.css and custom styles

--- View.elm
+++ View.elm
@@ -4,10 +4,16 @@ import Update
 import Model
 import Html
 import Html.Events
+import Html.Attributes
 
 
 view : Model.Model -> Html.Html Update.Msg
 view model =
     Html.div
-        [ Html.Events.onClick Update.ChangeText ]
-        [ Html.text model.text ]
+        [ Html.Attributes.class "skeleton-elm-project" ]
+        [ Html.div
+            [ Html.Attributes.class "waves-effect waves-light btn-large"
+            , Html.Events.onClick Update.ChangeText
+            ]
+            [ Html.text model.text ]
+        ]
