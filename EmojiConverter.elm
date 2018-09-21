e722fed0ca0f95219481b844c16047f9bff3b664
Tessa Kelly <tessakkelly@gmail.com>
2018-09-21 13:34:54 -0700

    :art: avoid referencing regex types in the type signatures we do not really care that regex is involved so much as we care about what strings we are working with

--- EmojiConverter.elm
+++ EmojiConverter.elm
@@ -14,26 +14,30 @@ type alias Key =
 
 textToEmoji : Key -> String -> String
 textToEmoji key text =
-    convert supportedLetters (rotateEmojis key) everyCharacter text
-
-
-everyCharacter : Regex.Regex
-everyCharacter =
-    Maybe.withDefault Regex.never (Regex.fromString ".")
+    convert supportedLetters (rotateEmojis key) splitEveryCharacter text
 
 
 emojiToText : Key -> String -> String
 emojiToText key emojis =
-    let
-        splitter =
-            Maybe.withDefault Regex.never <|
-                -- due to JavaScript issues with splitting and unicode, we maually split the string.
-                Regex.fromString "([\\uD800-\\uDBFF][\\uDC00-\\uDFFF])"
-    in
-    convert (rotateEmojis key) supportedLetters splitter emojis
+    convert (rotateEmojis key) supportedLetters splitEveryEmoji emojis
 
 
-convert : List String -> List String -> Regex.Regex -> String -> String
+splitEveryCharacter : String -> List String
+splitEveryCharacter =
+    Regex.fromString "."
+        |> Maybe.withDefault Regex.never
+        |> Regex.split
+
+
+splitEveryEmoji : String -> List String
+splitEveryEmoji =
+    -- due to JavaScript issues with splitting and unicode, we maually split the string.
+    Regex.fromString "([\\uD800-\\uDBFF][\\uDC00-\\uDFFF])"
+        |> Maybe.withDefault Regex.never
+        |> Regex.split
+
+
+convert : List String -> List String -> (String -> List String) -> String -> String
 convert orderedKeys orderedValues splitter string =
     let
         lookupTable =
@@ -46,7 +50,7 @@ convert orderedKeys orderedValues splitter string =
                 |> Maybe.withDefault key
     in
     string
-        |> Regex.split splitter
+        |> splitter
         |> List.map getValueOrReturnKey
         |> String.join ""
 
